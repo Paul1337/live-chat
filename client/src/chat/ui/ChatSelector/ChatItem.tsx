fc27fd42721dc9f1ac04dd04af9d9559fffaedc7
Paul <p@x128.ru>
2023-12-10 22:20:41 +0300

    upd

--- client/src/chat/ui/ChatSelector/ChatItem.tsx
+++ client/src/chat/ui/ChatSelector/ChatItem.tsx
@@ -1,24 +1,31 @@
 import classNames from 'classnames';
-import React, { FC } from 'react';
+import React, { CSSProperties, FC } from 'react';
 
 interface ChatItemProps {
     chatName: string;
+    photo?: string;
     isSelected: boolean;
     onClick: () => void;
 }
 
+const DefaultPhoto = 'default_profile.png';
+
 export const ChatItem: FC<ChatItemProps> = props => {
-    const { chatName, onClick, isSelected } = props;
+    const { chatName, onClick, isSelected, photo = DefaultPhoto } = props;
+    const photoStyle: CSSProperties = {
+        backgroundImage: `url(./src/assets/${photo})`,
+    };
     return (
         <div
             onClick={onClick}
             className={classNames(
-                'border-blue-500 border-2 rounded-md p-2 m-2 hover:bg-slate-400 cursor-pointer mt-4',
+                'flex justify-between items-center border-blue-500 border-2 rounded-md p-2 m-2 hover:bg-slate-400 cursor-pointer mt-4',
                 {
                     'border-red-500': isSelected,
                 }
             )}
         >
+            <div className='w-8 h-8 bg-cover bg-center' style={photoStyle}></div>
             <h1 className='text-xl'>{chatName}</h1>
         </div>
     );
--- client/src/chat/ui/MessageInput/MessageInput.tsx
+++ client/src/chat/ui/MessageInput/MessageInput.tsx
@@ -3,9 +3,10 @@ import React from 'react';
 export const MessageInput = () => {
     return (
         <div className='flex flex-col w-full '>
-            <textarea className=' w-full flex-1 rounded-md p-4 border border-black'>
-                MessageInput
-            </textarea>
+            <textarea
+                className=' w-full flex-1 rounded-md p-4 border border-black'
+                placeholder='Enter new message'
+            ></textarea>
             <button className='hover:bg-slate-300 p-2 rounded-md w-52 mt-2'>Send</button>
         </div>
     );
