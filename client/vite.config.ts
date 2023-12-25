3ee48fb7c28a9c7c66b91da2f58367f763b95ef0
Paul <p@x128.ru>
2023-12-25 10:50:47 +0300

    config updates

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    // base: '.',
-    plugins: [react()],
-    build: {
-        outDir: '../server/static',
-        emptyOutDir: false,
-    },
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), 'VITE');
+    return {
+        // base: '.',
+        plugins: [react()],
+        build: {
+            outDir: '../server/static',
+            emptyOutDir: false,
+        },
+        server: {
+            port: Number(env.VITE_DEV_PORT) ?? 8001,
+        },
+    };
 });
